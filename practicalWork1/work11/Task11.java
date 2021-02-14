babe70dc530bf1de27da174d49568f1e148d74ce
dsyniakin <61972076+dsyniakin@users.noreply.github.com>
2021-02-14 10:12:36 +0200

    Add files via upload

---
+++ practicalWork1/work11/Task11.java
@@ -0,0 +1,7 @@
+package practicalWork1.work11;
+
+public class Task11 {
+    public static void main(String[] args) {
+        System.out.println("Hello student!");
+    }
+}
---
+++ practicalWork1/work12/Task12.java
@@ -0,0 +1,13 @@
+package practicalWork1.work12;
+
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name");
+
+        String name = scan.nextLine();
+        System.out.println("My name is " + name + ".");
+    }
+}
